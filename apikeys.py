16285d73773b6c34153b2e5932f9a38014db4776
Rishabh Shah <35496282+rishabhshah13@users.noreply.github.com>
2024-03-21 01:59:14 -0400

    Delete apikeys.py

--- apikeys.py
+++
@@ -1,5 +0,0 @@
-# Description: This file contains the API keys for the API calls to OpenAI gpt-3.5-turbo or gpt-4.
-apikey_list = [
-    # 'sk-1xxxxxxxxxxxxxxxxx',
-    # 'sk-2xxxxxxxxxxxxxxxxx',
-]
